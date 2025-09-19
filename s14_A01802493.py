20f0c420ba287bad3380ae9e0bd267c34d09d619
lu ngz <a01802493@tec.mx>
2025-09-19 07:57:57 -0600

    Update s14_A01802493.py

--- s14_A01802493.py
+++ s14_A01802493.py
@@ -1,1 +1,17 @@
+#script that asks the user for their age and determines if they are an adult or not
+age = int(input("How old are you"))
+if age < 100 and age > 17:
+    print("great")
+else:print("age not valid, you are not an adult")   
+  
+#script that asks the user's age and determines if they are a teenager or not
+age = int(input("how old are you"))
+if age <20 and age > 12: 
+ print("great, you are a teenager")
+else:print("age not valid, you are not a teenager")
 
+#script that determines the user's name by asking for their birth year
+year = int(input("In what year were you born?"))
+birthday = int(input("have you had your birthday this year?"))
+if birthday: yes: print (2025 - year)
+if birthday: no: print (2025 - year - 1)
